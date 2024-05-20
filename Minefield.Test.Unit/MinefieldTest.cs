5ca1d42700f02e407a54673e000d48b250b55e62
Niklas <niklas.frank@gmail.com>
2024-05-20 14:36:29 +0200

    test: remove string formating from test assertion

--- Minefield.Test.Unit/MinefieldTest.cs
+++ Minefield.Test.Unit/MinefieldTest.cs
@@ -8,26 +8,26 @@ public class MinefieldTest
         get
         {
             return new[] {
-               new object[] {
-                 (3,3),
-                 new (int, int)[]{},
-                 new []{Minefield.Reveal(0, 0)},
-                 @"You Won
-                   __012
-                   2 ...
-                   1 ...
-                   0 ...",
-                 "No bombs should win the game"
-               },
+                new object[] {
+                  (3,3),
+                  new (int, int)[]{},
+                  new []{Minefield.Reveal(0, 0)},
+                  @"You Won
+                  __012
+                  2 ...
+                  1 ...
+                  0 ...",
+                  "No bombs should win the game"
+                },
                 new object[] {
                   (3,3),
                   new []{(0,0), (0,1), (0,2), (1,0), (1,2), (2,0), (2,1), (2,2)},
                   new []{Minefield.Reveal(1, 1)},
                   @"You Won
-                    __012
-                    2 ???
-                    1 ?8?
-                    0 ???",
+                  __012
+                  2 ???
+                  1 ?8?
+                  0 ???",
                   "Picking all the bombs should win the game"
                 },
                 new object[] {
@@ -35,54 +35,54 @@ public class MinefieldTest
                   new []{(0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2)},
                   new []{Minefield.Reveal(1, 1)},
                   @"You Lost
-                    __012
-                    2 QQQ
-                    1 QXQ
-                    0 QQQ",
+                  __012
+                  2 QQQ
+                  1 QXQ
+                  0 QQQ",
                   "Picking a bomb explodes and reveals all other bombs left, loosing the game"
                 },
                 new object[] {
-                   (3,4),
-                   new []{(0,2), (1, 2), (2,2)},
-                   new []{Minefield.Reveal(0, 0)},
-                   @"
-                     __012
-                     3 ???
-                     2 ???
-                     1 232
-                     0 ...",
-                   "a move should not open up past bombs"
-                 },
+                  (3,4),
+                  new []{(0,2), (1, 2), (2,2)},
+                  new []{Minefield.Reveal(0, 0)},
+                  @"
+                  __012
+                  3 ???
+                  2 ???
+                  1 232
+                  0 ...",
+                  "a move should not open up past bombs"
+                },
                 new object[] {
-                   (3,4),
-                   new []{(0,3), (1, 3), (2,3)},
-                   new []{Minefield.Reveal(0, 0)},
-                   @"You Won
-                     __012
-                     3 ???
-                     2 232
-                     1 ...
-                     0 ...",
-                   "selecting an empty square should open up the board"
-                 },
+                  (3,4),
+                  new []{(0,3), (1, 3), (2,3)},
+                  new []{Minefield.Reveal(0, 0)},
+                  @"You Won
+                  __012
+                  3 ???
+                  2 232
+                  1 ...
+                  0 ...",
+                  "selecting an empty square should open up the board"
+                },
                 new object[] {
-                   (3,3),
-                   new []{(0,2), (1, 2), (2,2)},
-                   new []{Minefield.Reveal(0, 0)},
-                   @"You Won
-                     __012
-                     2 ???
-                     1 232
-                     0 ...",
-                   "revealed squares count adjacent bombs"
-                 },
+                  (3,3),
+                  new []{(0,2), (1, 2), (2,2)},
+                  new []{Minefield.Reveal(0, 0)},
+                  @"You Won
+                  __012
+                  2 ???
+                  1 232
+                  0 ...",
+                  "revealed squares count adjacent bombs"
+                },
                 new object[] {
                   (1,1),
                   new []{(0,0)},
                   new []{Minefield.Flag(0,0)},
                   @"You Won
-                    __0
-                    0 F",
+                  __0
+                  0 F",
                   "flagging all bombs should win the game"
                 },
                 new object[] {
@@ -90,9 +90,9 @@ public class MinefieldTest
                   new []{(0,0), (2,2)},
                   new []{Minefield.Flag(0,0)},
                   @"
-                    __01
-                    1 ??
-                    0 F?",
+                  __01
+                  1 ??
+                  0 F?",
                   "flagging should not end the game"
                 },
                 new object[] {
@@ -100,9 +100,9 @@ public class MinefieldTest
                   new []{(0,0), (2,2)},
                   new []{Minefield.Flag(0,0), Minefield.Flag(0,0)},
                   @"
-                    __01
-                    1 ??
-                    0 ??",
+                  __01
+                  1 ??
+                  0 ??",
                   "flagging again should unflag the cell"
                 },
                 new object[] {
@@ -110,11 +110,11 @@ public class MinefieldTest
                   new [] {(2,3),(1,2),(3,3)},
                   new [] {Minefield.Reveal(0,0)},
                   @"
-                    __0123
-                    3 ????
-                    2 ??32
-                    1 111.
-                    0 ....",
+                  __0123
+                  3 ????
+                  2 ??32
+                  1 111.
+                  0 ....",
                   "a move doesnt open up past non empty squares"
                 },
                 new object[] {
@@ -122,11 +122,11 @@ public class MinefieldTest
                   new [] {(2,3),(1,2),(3,3)},
                   new [] {Minefield.Reveal(0,0), Minefield.Flag(1, 2), Minefield.Reveal(3,3)},
                   @"You Lost
-                    __0123
-                    3 ??QX
-                    2 ?B32
-                    1 111.
-                    0 ....",
+                  __0123
+                  3 ??QX
+                  2 ?B32
+                  1 111.
+                  0 ....",
                   "all parts combined should work as a whole"
                 },
             };
@@ -142,7 +142,7 @@ public class MinefieldTest
             mf.MakeMove(m);
         }
         var actual = WithStringWriter(mf.Render);
-        Assert.AreEqual(BoardToString(expected), actual, message);
+        Assert.AreEqual(NormalizeMultilineStrings(expected), NormalizeMultilineStrings(actual), message);
     }
 
     [TestMethod]
@@ -152,25 +152,25 @@ public class MinefieldTest
         Assert.ThrowsException<MinefieldInvalidSizeException>(() => new Minefield(0, -1, bombs));
         Assert.ThrowsException<MinefieldInvalidSizeException>(() => new Minefield(-1, 0, bombs));
 
-        var expected = BoardToString(@"
-            __01234567
-            9 ????????
-            8 ????????
-            7 ????????
-            6 ????????
-            5 ????????
-            4 ????????
-            3 ????????
-            2 ????????
-            1 ????????
-            0 ????????");
+        var expected = @"
+          __01234567
+          9 ????????
+          8 ????????
+          7 ????????
+          6 ????????
+          5 ????????
+          4 ????????
+          3 ????????
+          2 ????????
+          1 ????????
+          0 ????????";
 
         var mf = new Minefield(8, 10, bombs);
         var actual = WithStringWriter(mf.Render);
-        Assert.AreEqual(expected, actual);
+        Assert.AreEqual(NormalizeMultilineStrings(expected), NormalizeMultilineStrings(actual));
     }
 
-    private string BoardToString(string board) => String.Join(Environment.NewLine, board.Split(Environment.NewLine).Select(s => s.Trim()));
+    public static string NormalizeMultilineStrings(string input) => String.Join(Environment.NewLine, input.Replace("\r\n", Environment.NewLine).Replace("\r", Environment.NewLine).Split(Environment.NewLine).Select(s => s.TrimStart()));
     private string WithStringWriter(Action<TextWriter> fn)
     {
         using (var sw = new StringWriter())
